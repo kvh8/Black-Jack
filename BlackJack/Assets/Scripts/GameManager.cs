ba7fe6e3da8e07cdc9eb7fb30c9af267cae9a874
Kierstyn <kvh8@humboldt.edu>
2020-02-12 23:30:00 -0800

    More script for game manager

--- BlackJack/Assets/Scripts/GameManager.cs
+++ BlackJack/Assets/Scripts/GameManager.cs
@@ -4,6 +4,29 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    [SerializeField]
+    private GameObject[] cardPrefabs, playerCardPosition, dealerCardPosition, deckCardPosition;
+    [SerializeField]
+    private GameObject backCardPrefab;
+    [SerializeField]
+    private Button hitDeal, stand, resetBalance, betFiveHundred, betOneHundred, betFifty;
+    [SerializeField]
+    private Text textMoney, textBet, textPlayerPoints, textDealerPoints, textPlaceYourBet, textSelectingBet, textWinner;
+    [SerializeField]
+    private Image resetImgBtn;
+
+
+    
+
+
+
+
+
+
+
+
+
+
     // Start is called before the first frame update
     void Start()
     {
